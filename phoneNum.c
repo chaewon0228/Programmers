bae4c3c29db2ddd0ffc7bd064eed64d1df6afe7c
chaewon0228 <s2033@e-mirim.hs.kr>
2021-07-24 23:58:52 +0900

    [Add] 전화번호 가리기 수정

--- phoneNum.c
+++ phoneNum.c
@@ -4,11 +4,14 @@
 #include <string.h>
 
 char* solution(char* phone_number) {
-    char* answer = (char*)malloc(1);
-    strcpy(answer, phone_number);
+    char* answer = (char*)malloc(12);
+    for (int i = 0; i < strlen(phone_number); i++) {
+        answer[i] = phone_number[i];
+    }
     for (int i = 0; i < strlen(phone_number) - 4; i++) {
         answer[i] = '*';
     }
+    answer[strlen(phone_number)] = NULL;
     return answer;
 }
 int main() {
