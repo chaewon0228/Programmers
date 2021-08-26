f1ae13019ef0a6e13820bfb953cd7116ba431372
chaewon0228 <s2033@e-mirim.hs.kr>
2021-08-26 22:56:06 +0900

    [Add] 문자열을 정수로 바꾸기

---
+++ StringAsInteger.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+int solution(const char* s) {
+    int answer = 0;
+    answer = atoi(s);
+    return answer;
+}
+
+int main() {
+    printf("solution함수의 반환값은 %d \n", solution("1234"));
+    printf("solution함수의 반환값은 %d \n", solution("-1234"));
+    return 0;
+}
