6ebc88202e4d1a3df2f446f4f0a4b7fa66246cc5
ChaeWonYun <58872725+chaewon0228@users.noreply.github.com>
2021-08-29 23:56:07 +0900

    [Add] 문자열을 내림차순으로 배치하기

--- StrDescendingOrder.cpp
+++ StrDescendingOrder.cpp
@@ -3,9 +3,7 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
-// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
 char* solution(const char* s) {
-    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
     char* answer = (char*)malloc(strlen(s) * sizeof(char));
     strcpy(answer, s);
     char b;
@@ -21,6 +19,6 @@ char* solution(const char* s) {
     return answer;
 }
 int main() {
-    printf("Zbcdefg의 내림차수 정렬의 값은 %s 입니다.\n", solution("Zbcdefg"));
+    printf("Zbcdefg -> %s \n", solution("Zbcdefg"));
     return 0;
 }
