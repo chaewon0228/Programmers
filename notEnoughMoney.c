f420079932676db6430e7d80490165d6bb7c1477
YunChaeWon <58872725+chaewon0228@users.noreply.github.com>
2021-10-07 23:44:18 +0900

    [Add] 1주차_부족한 금액 계산하기

---
+++ notEnoughMoney.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+long long solution(int price, int money, int count) {
+    long long result = money;
+    for(int i = 1; i <= count; i++) result -= price * i;
+    if(result >= 0) return 0;
+    return result *= -1;
+}
+int main(){
+    int price = 3;
+    int money = 20; 
+    int count = 4;
+    printf("이용료가 %d인 놀이기구를 %d번 탔을 경우 부족한 금액은 %d이다.", price, count, money, solution(price, money, count));
+    return 0;
+}
