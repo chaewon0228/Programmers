5a68739912b773705571d78c768bb9dbb2fe9c1f
YunChaeWon <58872725+chaewon0228@users.noreply.github.com>
2021-11-05 23:59:47 +0900

    [Add] 부족한 금액 계산하기

--- notEnoughMoney.c
+++ notEnoughMoney.c
@@ -4,7 +4,8 @@
 
 long long solution(int price, int money, int count) {
     long long result = money;
-    for(int i = 1; i <= count; i++) result -= price * i;
+    for(int i = 1; i <= count; i++)
+        result -= price * i;
     if(result >= 0) return 0;
     return result *= -1;
 }
@@ -12,6 +13,6 @@ int main(){
     int price = 3;
     int money = 20; 
     int count = 4;
-    printf("이용료가 %d인 놀이기구를 %d번 탔을 경우 부족한 금액은 %d이다.", price, count, money, solution(price, money, count));
+    printf("이용료 : %d, %d번 탔을 경우 부족한 금액은 %d이다.", price, count, money, solution(price, money, count));
     return 0;
 }
