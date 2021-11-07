37923fb3c07958965dcf1e56408c834c706322d3
chaewon0228 <s2033@e-mirim.hs.kr>
2021-11-07 23:34:39 +0900

    [Add] 없는 숫자 더하기

---
+++ notExistSum.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// numbers_len은 배열 numbers의 길이입니다.
+int solution(int numbers[], size_t numbers_len) {
+    int answer = 0;
+    bool tt[10] = { false, };
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < numbers_len; j++) {
+            if (i == numbers[j]) tt[i] = true;
+        }
+    }
+    for (int i = 0; i < 10; i++) {
+        if (tt[i] == false) answer += i;
+    }
+    return answer;
+}
+/*
+int solution(int numbers[], size_t numbers_len) {
+    int answer = 0;
+
+    for(int i=0; i<numbers_len; i++){
+        answer += numbers[i];
+    }
+    return 45 - answer;
+}
+*/
+int main() {
+    int numbers[] = { 1,2,3,4,6,7,8,0 };
+    int numbers_len = 8;
+    printf("[1,2,3,4,6,7,8,0] => %d", solution(numbers, numbers_len));
+    return 0;
+}
